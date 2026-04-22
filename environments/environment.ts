96a06bb6ed444f2d36b9bf8117991f697a54333f
tianyaqpzm <tianyaqpzm@163.com>
2026-04-22 23:14:01 +0000

    【xxx】修改为nginx的配置8443

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    VITE_API_URL: "https://tao-lan.122577.xyz:8281",
-    VITE_GATEWAY_URL: "https://tao-lan.122577.xyz:8281"
+    VITE_API_URL: "https://tao-lan.122577.xyz:8443",
+    VITE_GATEWAY_URL: "https://tao-lan.122577.xyz:8443"
 };
