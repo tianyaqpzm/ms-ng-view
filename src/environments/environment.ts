7b5482437d5b678663fd02dd9e7c041db12090f5
tianyaqpzm <tianyaqpzm@163.com>
2026-05-06 01:25:54 +0000

    chore: remove port number from API and gateway URLs in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    VITE_API_URL: "https://dark.122577.xyz:8443",
-    VITE_GATEWAY_URL: "https://dark.122577.xyz:8443",
+    VITE_API_URL: "https://dark.122577.xyz",
+    VITE_GATEWAY_URL: "https://dark.122577.xyz",
     VITE_CASDOOR_URL: "https://casdoor.122577.xyz"
 };
